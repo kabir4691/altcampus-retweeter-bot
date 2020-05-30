f0ae2dec2cb959c9e619fc5526b19c68db4145c9
Kabir <kabir4691@gmail.com>
2020-05-30 11:13:19 +0530

    Revert "Add wake controller"
    This reverts commit 9512d1c1ea4d46ecbda377b8143358b8a46a1e25.

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const wakeController = require('./controller/wakeController');
 const retweetController = require('./controller/retweetController');
 const likeController = require('./controller/likeController');
 
-wakeController.start();
 retweetController.start();
 likeController.start();
