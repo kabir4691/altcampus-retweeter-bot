c4d5dc9b1fcd9b74bbc4bf829dcfc48a6b498a9b
Kabir <kabir4691@gmail.com>
2020-05-30 20:01:28 +0530

    Add express app to listen on port 4000

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const retweetController = require('./controller/retweetController');
 const likeController = require('./controller/likeController');
+const app = require('express')();
 
 retweetController.start();
 likeController.start();
+
+app.listen(4000, () => {
+  console.log('Your app is listening on port 4k');
+})
+
