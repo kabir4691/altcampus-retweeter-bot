56482f00cf227826acef757d1d6358b2fb2f76be
Kabir <kabir4691@gmail.com>
2020-05-31 16:21:36 +0530

    Add morgan for API logging

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const retweetController = require('./controller/retweetController');
 const likeController = require('./controller/likeController');
 const app = require('express')();
+const morgan = require('morgan');
 
 retweetController.start();
 likeController.start();
+
+app.use(morgan('dev'));
 
 app.listen(4000, () => {
   console.log('Your app is listening on port 4k');
