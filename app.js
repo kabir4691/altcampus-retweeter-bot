bc9f64b4b11150d1cf634638727b8173ed542768
Kabir <kabir4691@gmail.com>
2020-05-31 16:23:02 +0530

    Add express middleware for / route
    Added a middleware for GET / for Uptime Robot monitoring purposes.

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ likeController.start();
 
 app.use(morgan('dev'));
 
+app.get('/', (_, res) => {
+  res.sendStatus(200);
+});
+
 app.listen(4000, () => {
   console.log('Your app is listening on port 4k');
 })
